f63a0d64a7cea0067dd1354cd7655ab633fba1a8
Priyavadhana M <priyavadhanam@Priyavadhanas-MacBook-Air.local>
2024-09-19 11:50:43 +0530

    Your commit message

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,49 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { RouterModule,Routes } from '@angular/router';
+import { LogoutComponent } from './logout/logout.component';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule}  from '@angular/common/http';
+import { ProductsComponent } from './products/products.component';
+import { ContactComponent } from './contact/contact.component'
+
+const routes:Routes =[ 
+  {path: '', component: HomeComponent},
+  {path: 'login', component: LoginComponent},
+  {path: 'register', component: RegisterComponent},
+  {path: 'dashboard', component: DashboardComponent},
+  {path: 'products', component: ProductsComponent },
+  {path: 'contact', component: ContactComponent},
+  {path: 'home', component: HomeComponent}
+  
+  
+
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    NavbarComponent,
+    LoginComponent,
+    RegisterComponent,
+    DashboardComponent,
+    LogoutComponent,
+    ProductsComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+     RouterModule.forRoot(routes),
+     FormsModule,
+     HttpClientModule
   ],
   providers: [
     provideClientHydration()
